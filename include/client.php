38aafbd31e73f3e3def0d7eefbb8c7de6dd7d91b
KASSOUMH <hadykassoum2003@gmail.com>
2025-03-18 16:01:10 +0100

    client

--- include/client.php
+++ include/client.php
@@ -1,2 +1,15 @@
-<?php
-echo "<h1>Bonjour client</h1>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Example</title>
+    <link rel="stylesheet" href="./css/index.css"> 
+</head>
+<body>
+
+    <?php
+    echo "<h1>Bonjour client</h1>";?>
+
+</body>
+</html>
