2fccdaa393cb30fb6bda9c50759359e828a4a439
KASSOUMH <hadykassoum2003@gmail.com>
2025-03-18 15:26:33 +0100

    new

--- include/pageCon.php
+++ include/pageCon.php
@@ -9,7 +9,7 @@
       <input name='nom' type='text' placeholder='Entrer votre nom' id='nom' required> <br><br>
       
       <label for='mdp'><b>Mot de passe :</b></label>
-      <input name='mdp' type='password' placeholder='Entrer votre mot de passe' id='mdp' required> <br> </div>"
+      <input name='mdp' type='password' placeholder='Entrer votre mot de passe' id='mdp' required> <br> </div>";
 
       echo "<button type='submit' id='submit-btn'>Connexion</button> <br><br>"
     ?>
