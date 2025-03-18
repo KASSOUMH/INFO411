2cbdf8ed3dd949bd3ab3addbf554b9da6d0de732
KASSOUMH <hadykassoum2003@gmail.com>
2025-03-18 14:54:21 +0100

    banner

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <?php
         echo "<h1>Bonjour</h1>";
         echo "<div class='banner'>
-        <img src='../image/logo.jpeg' alt='Logo'>
+        <p> Welcome to the database </p>
         </div>";
     ?>
 </body>
