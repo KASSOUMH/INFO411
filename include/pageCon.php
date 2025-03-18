c101ed67b2358366f439567b5aeb86763159bbf9
KASSOUMH <hadykassoum2003@gmail.com>
2025-03-18 15:25:02 +0100

    new

--- include/pageCon.php
+++ include/pageCon.php
@@ -3,4 +3,11 @@
         echo "<div class='banner'>
         <p> Welcome to the database </p>
         </div>";
+        echo "<div class='container'>
+    <form method='POST' action='pageCon.php'>
+      <label for='nom'><b>Nom :</b></label>
+      <input name='nom' type='text' placeholder='Entrer votre nom' id='nom' required> <br><br>
+      
+      <label for='mdp'><b>Mot de passe :</b></label>
+      <input name='mdp' type='password' placeholder='Entrer votre mot de passe' id='mdp' required> <br> </div>"
     ?>
