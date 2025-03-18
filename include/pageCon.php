ac64815e3196e77024ad69fda568b0360241263a
KASSOUMH <hadykassoum2003@gmail.com>
2025-03-18 21:43:30 +0100

    css

--- include/pageCon.php
+++ include/pageCon.php
@@ -5,7 +5,7 @@ include("connexion.php");
 
 echo "<h1>Bonjour</h1>";
 echo "<div class='banner'>
-<p> Welcome to the database </p>
+<p class='transparent'> Welcome to the database </p>
 </div>";
 
 
